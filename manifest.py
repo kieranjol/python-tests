490f8385bbdd1ceda1fad3db82c7ce25a4c923fd
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-12-16 01:00:38 +0000

    manifest: write2file

--- manifest.py
+++ manifest.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 import os
 filename = sys.argv[1]
+manifest = sys.argv[1] + 'manifest.md5'
 
 dirname = os.path.dirname(filename)
 print dirname
@@ -9,4 +10,7 @@ os.chdir(dirname)
 dingo = os.path.normpath(filename)
 var = dingo.split(os.sep)
 print var[-1]
-subprocess.call(['md5deep', '-ler', var[-1]])
+manimani = subprocess.check_output(['md5deep', '-ler', var[-1]])
+
+with open(manifest,"w+") as fo:
+	fo.write(manimani)
