7bc687adfa7a05735e7be836d4da8992809ff1a3
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-04-26 23:17:18 +0100

    audit.py add csv/mediainfo stuff

--- audit.py
+++ audit.py
@@ -3,18 +3,39 @@ import sys
 import filecmp
 from glob import glob
 import os
-from Tkinter import *
-import tkFileDialog
-root = Tk()
+import time
+import csv
 
-# Create file-open dialog.
-root.update()
+video_dir = sys.argv[1]
+def create_csv(csv_file, *args):
+    f = open(csv_file, 'wb')
+    try:
+        writer = csv.writer(f)
+        writer.writerow(*args)
+    finally:
+        f.close()
+        
+def append_csv(csv_file, *args):
+    f = open(csv_file, 'ab')
+    try:
+        writer = csv.writer(f)
+        writer.writerow(*args)
+    finally:
+        f.close()
+csv_filename = os.path.basename(video_dir) +  '_item_level' + time.strftime("_%Y_%m_%dT%H_%M_%S")
+# CSV will be saved to your Desktop.
+csvfile = os.path.expanduser("~/Desktop/%s.csv") % csv_filename       
+create_csv(csvfile,('FILENAME', 'CODEC'))      
 # Directory with files that we want to transcode losslessly and generate metadata for
-video_dir = tkFileDialog.askdirectory(parent=root)
+
 #csv       = tkFileDialog.asksaveasfile(parent=root, defaultextension='.csv') 
 for root, dirs, files in os.walk(video_dir):
     
     for filename in files:
         
-        if filename.endswith(('.mov', '.mxf', '.avi', '.mp4')):
-             print(os.path.join(root, filename))
+        if filename.endswith(('.mov', '.mxf', '.avi', '.mp4', '.ogg', '.ogm', '.wmv','.mkv','.flv','.webm')):
+             name = (os.path.join(root, filename))
+             codec = subprocess.check_output(['mediainfo','--Language=raw','--Full',"--Inform=Video;%Codec%", filename]).rstrip()
+             print codec
+             print name
+             append_csv(csvfile, (name,codec))
