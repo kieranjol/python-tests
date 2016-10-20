b24e68704b6e4a1441a52f3c850d8cc014024f7f
kieranjol <kieran.o.leary@gmail.com>
2016-10-20 19:35:14 +0100

    pbcolloton - initial commit

---
+++ pbcolloton.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import os
+import subprocess
+
+video_dir = sys.argv[1]
+for root, dirs, files in os.walk(video_dir):
+    for filename in files:
+        if filename.endswith(('.mov', '.mxf', '.avi', '.mp4', '.ogg', '.ogm', '.wmv','.mkv','.flv','.webm')):
+            full_path = os.path.join(root,filename)
+            cmd = ['mediainfo', '--Output=PBCore2', full_path]
+            metadata = subprocess.check_output(cmd)
+            sidecar = filename + '.xml'
+            print 'Creating PBCore sidecar for %s' % filename 
+            with open(sidecar, 'wb') as fo:
+                fo.write(metadata)
